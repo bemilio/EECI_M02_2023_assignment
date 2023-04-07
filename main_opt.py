2f9e2a625e186a4ccd1b5c5b470f9f80885b249e
Emilio Benenati <e.benenati@tudelft.nl>
2023-04-07 17:17:28 +0200

    implemented robust Newton-raphson

--- main_opt.py
+++ main_opt.py
@@ -5,9 +5,11 @@ from tvopt import distributed_solvers as ds
 from tvopt import costs
 import numpy as np
 from mycosts import TwoExpCost
-from my_distr_solvers import admm_fix, newton_raphson
+from my_distr_solvers import admm_fix, newton_raphson, robust_newton_raphson
 from numpy import random
 import matplotlib.pyplot as plt
+import scipy.optimize as opt
+
 plt.rcParams.update({
     "text.usetex": True,
     "font.family": "STIXGeneral",
@@ -24,59 +26,108 @@ if __name__ == '__main__':
     ###
     random.seed(1)
     N=2
-    N_it = 1000
+    N_it = 100
     adj_mat = nw.random_graph(N, 0.1)
     net = nw.Network(adj_mat)
 
     f_i = []
+    a = []
+    b = []
+    c = []
+    d = []
     for i in range(N):
-        a = 0.2* random.rand()
-        b = .2* random.rand()
-        c = random.rand()
-        d = random.rand()
+        a.append(0.2* random.rand())
+        b.append(.2* random.rand())
+        c.append(random.rand())
+        d.append(random.rand())
 
-        f_i.append(TwoExpCost(a,b,c,d))
+        f_i.append(TwoExpCost(a[i],b[i],c[i],d[i]))
     f=costs.SeparableCost(f_i)
 
+    f_centralized = lambda x: sum([f_i[i].function(x) for i in range(N)])
+    res = opt.minimize(f_centralized, 0)
+
     problem = {"f": f, "network": net}
-    x_admm = np.zeros((1, N, N_it+1))
-    x_dpg = np.zeros((1, N, N_it+1))
-    x_NR = np.zeros((1, N, N_it+2))
-    f_val_admm = np.zeros(N_it)
-    f_val_dpg = np.zeros(N_it)
-    f_val_NR = np.zeros(N_it)
+    # Initialization
+    #ADMM
+    x_admm = np.zeros((N, N_it+1))
     z = {}
-    y_NR = np.zeros((1,N))
-    s_NR = np.zeros((1, N))
-    g_NR = np.zeros((1,N))
-    h_NR = np.zeros((1, N))
     for i in range(net.N):
         for j in net.neighbors[i]:
             z[i, j] = 0
+    f_val_admm = np.zeros(N_it)
+    # Grad
+    x_dpg = np.zeros((N, N_it+1))
+    f_val_dpg = np.zeros(N_it)
+    # Newton-Raphson
+    x_NR = np.zeros((N, N_it+1))
+    y_NR = np.zeros((N))
+    s_NR = np.zeros((N))
+    g_NR = np.zeros((N))
+    h_NR = np.zeros((N))
+    f_val_NR = np.zeros(N_it)
+    # Robust Newton-Raphson
+    x_rNR = np.zeros((N, N_it + 1))
+    y_rNR = np.zeros((N))
+    s_rNR = np.ones((N))
+    g_rNR = np.zeros((N))
+    h_rNR = np.ones((N))
+    g_old_rNR = np.zeros((N))
+    h_old_rNR = np.ones((N))
+    f_val_rNR = np.zeros(N_it)
+    sigma_y_rNR = np.zeros((N))
+    sigma_s_rNR = np.zeros((N))
+    rho_y_rNR = {}
+    for i in range(net.N):
+        for j in net.neighbors[i]:
+            rho_y_rNR[i, j] = 0
+    rho_s_rNR = {}
+    for i in range(net.N):
+        for j in net.neighbors[i]:
+            rho_s_rNR[i, j] = 0
+
     for t in range(N_it):
-        x_admm[..., t+1], z = admm_fix(problem, z_0=z,  penalty=1, rel=.5, num_iter=1)
+        # ADMM
+        x_admm[..., t+1], z = admm_fix(problem, z_0=z,  penalty=.1, rel=.5, num_iter=1)
         f_val_admm[t] = sum(f.function(x_admm[...,t]))
-        x_dpg[..., t + 1] = tvopt.distributed_solvers.dpgm(problem, step=0.1, x_0=x_dpg[..., t], num_iter=1)
+        # Gradient
+        x_dpg[..., t + 1] = tvopt.distributed_solvers.dpgm(problem, step=0.5, x_0=x_dpg[..., t], num_iter=1)
         f_val_dpg[t] = sum(f.function(x_dpg[..., t]))
+        # Newton-Raphson
         x_NR[..., t+1], y_NR[:], s_NR[:], g_NR[:], h_NR[:] = \
-            newton_raphson(problem, step=0.02, x_0=x_NR[..., t], y_0=y_NR, s_0=s_NR, g_0=g_NR, h_0=h_NR, num_iter=1)
+            newton_raphson(problem, step=0.2, x_0=x_NR[..., t], y_0=y_NR, s_0=s_NR, g_0=g_NR, h_0=h_NR, num_iter=1)
         f_val_NR[t] = sum(f.function(x_NR[..., t]))
+        # Robust Newton-Raphson
+        x_rNR[..., t+1], y_rNR[:], s_rNR[:], g_rNR[:], h_rNR[:], \
+        g_old_rNR[:], h_old_rNR[:], sigma_y_rNR[:], sigma_s_rNR[:], rho_y_rNR, rho_s_rNR = \
+            robust_newton_raphson(problem, step=0.2, x_0=x_rNR[..., t], y_0=y_rNR, s_0=s_rNR, g_0=g_rNR, h_0=h_rNR,\
+                                  g_old_0=g_old_rNR, h_old_0=h_old_rNR, sigma_y_0=sigma_y_rNR, sigma_s_0=sigma_s_rNR, \
+                                  rho_y_0=rho_y_rNR, rho_s_0=rho_s_rNR, num_iter=1, p_packet_loss=0)
+        f_val_rNR[t] = sum(f.function(x_rNR[..., t]))
         print("Iteration " + str(t))
 
-    fig, ax = plt.subplots(1, 1, figsize=(6, 5.1))
-    ax.plot(range(N_it), f_val_admm, label="ADMM")
-    ax.set_ylabel(r'$f(x)$', fontsize=9)
-    ax.set_xlabel("Iteration", fontsize=9)
-    ax.grid()
-    ax.set_xlim(0, N_it)
-    ax.plot(range(N_it), f_val_dpg, label = "DG")
-    ax.set_ylabel(r'$f(x)$', fontsize=9)
-    ax.set_xlabel("Iteration", fontsize=9)
-    ax.plot(range(N_it), f_val_NR, label = "NR")
-    ax.set_ylabel(r'$f(x)$', fontsize=9)
-    ax.set_xlabel("Iteration", fontsize=9)
-    ax.grid()
-    ax.set_xlim(0, N_it)
+    fig, ax = plt.subplots(2, 1, figsize=(6, 5.1), sharex=True)
+    ax[0].plot(range(N_it), f_val_admm - res.fun, label="ADMM")
+    ax[0].plot(range(N_it), f_val_dpg - res.fun, label = "DG")
+    ax[0].plot(range(N_it), f_val_NR - res.fun, label = "NR")
+    ax[0].plot(range(N_it), f_val_rNR - res.fun, label = "rob. NR")
+    ax[0].set_ylabel(r'$f(x) - f^{\star}$', fontsize=9)
+    ax[0].set_xlabel("Iteration", fontsize=9)
+    ax[0].set_yscale('log')
+    ax[0].set_ylim([10**(-3), 10**0])
+    ax[0].grid()
+    # ax[0].set_yscale('log')
+    ax[0].set_xlim(0, N_it)
+    ax[1].plot(range(N_it), np.linalg.norm(x_admm - np.average(x_admm, axis=0), axis=0)[:-1], label="ADMM")
+    ax[1].plot(range(N_it), np.linalg.norm(x_dpg - np.average(x_dpg, axis=0), axis=0)[:-1], label = "DG")
+    ax[1].plot(range(N_it), np.linalg.norm(x_NR - np.average(x_NR, axis=0), axis=0)[:-1], label = "NR")
+    ax[1].plot(range(N_it), np.linalg.norm(x_rNR - np.average(x_rNR, axis=0), axis=0)[:-1], label = "rob. NR")
+    ax[1].set_ylabel(r'$\|x - \frac{1}{N}\sum{x_i}\|$', fontsize=9)
+    ax[1].set_xlabel("Iteration", fontsize=9)
+    ax[1].set_yscale('log')
+    ax[1].grid()
     plt.legend()
     plt.show(block=False)
+    fig.savefig(directory + '/Figures/Distr_opt.png')
+    fig.savefig(directory + '/Figures/Distr_opt.pdf')
     print("stop")
--- my_distr_solvers.py
+++ my_distr_solvers.py
@@ -1,5 +1,6 @@
 from tvopt import costs, sets, distributed_solvers
 import numpy as np
+from numpy import random
 
 def admm_fix(problem, penalty, rel, z_0=None, num_iter=100):
     r"""
@@ -104,8 +105,6 @@ def newton_raphson(problem, step, x_0=0, y_0=0, s_0=0, g_0=0, h_0=0, num_iter=10
     h_old[...] = h_0
     x_old = np.zeros(f.dom.shape)
 
-
-
     for l in range(int(num_iter)):
         for i in range(net.N):
             g[..., i] = f.hessian(x[...,i], i=i) * x[..., i] - f.gradient(x[...,i], i=i)
@@ -117,3 +116,63 @@ def newton_raphson(problem, step, x_0=0, y_0=0, s_0=0, g_0=0, h_0=0, num_iter=10
         x_old[:] = x
         x[:] = (1-step) * x + step * np.divide(y,s) if np.linalg.norm(s) >0.0001 else x
     return x, y, s, g, h
+
+def robust_newton_raphson(problem, step, x_0=0, y_0=0, s_0=1, g_0=0, h_0=1, g_old_0=0, h_old_0=1, \
+                          sigma_y_0=0, sigma_s_0=0, rho_y_0=None, rho_s_0=None, num_iter=100, p_packet_loss=0.1):
+    # unpack problem data
+    f, net = problem["f"], problem["network"]
+    weights = [1 / (1 + net.degrees[i]) for i in range(net.N)]
+
+    x = np.zeros(f.dom.shape)
+    y = np.zeros(f.dom.shape)
+    s = np.zeros(f.dom.shape)
+    g_old = np.zeros(f.dom.shape)
+    h_old = np.zeros(f.dom.shape)
+    g = np.zeros(f.dom.shape)
+    h = np.zeros(f.dom.shape)
+    sigma_y = np.zeros(f.dom.shape)
+    sigma_s = np.zeros(f.dom.shape)
+    x[...] = x_0
+    y[...] = y_0
+    s[...] = s_0
+    g[...] = g_0
+    h[...] = h_0
+    g_old[...] = g_old_0
+    h_old[...] = h_old_0
+    sigma_y[...] = sigma_y_0
+    sigma_s[...] = sigma_s_0
+    # initialize arc variables
+    rho_y = {}
+    rho_s = {}
+    for i in range(net.N):
+        for j in net.neighbors[i]:
+            if rho_y_0 is None:
+                rho_y[i, j] = np.zeros(f.dom.shape[:-1])
+            else:
+                rho_y[i, j] = rho_y_0[i, j]
+            if rho_s_0 is None:
+                rho_s[i, j] = np.zeros(f.dom.shape[:-1])
+            else:
+                rho_s[i, j] = rho_s_0[i, j]
+
+    for l in range(int(num_iter)):
+        i = np.random.randint(0, net.N)
+        y[..., i] = weights[i] * (y[..., i])
+        s[..., i] = weights[i] * (s[..., i])
+        sigma_y[..., i] = sigma_y[..., i] + y[..., i]
+        sigma_s[..., i] = sigma_s[..., i] + s[..., i]
+        for j in net.neighbors[i]:
+            if random.binomial(1,p_packet_loss)==0:
+                y[...,j] = sigma_y[..., i] - rho_y[i, j] + y[..., j]
+                s[...,j] = sigma_s[..., i] - rho_s[i, j] + s[..., j]
+                rho_y[i, j] = sigma_y[..., i]
+                rho_s[i, j] = sigma_s[..., i]
+                # update
+                g_old[..., j] = g[..., j]
+                h_old[..., j] = h[..., j]
+                x[..., j] = (1 - step) * x[..., j] + step * y[..., j] / s[..., j]
+                g[..., j] = f.hessian(x[..., j], i=j) * x[..., j] - f.gradient(x[..., j], i=j)
+                h[..., j] = f.hessian(x[..., j], i=j)
+                y[..., j] = y[..., j] + g[..., j] - g_old[..., j]
+                s[..., j] = s[..., j] + h[..., j] - h_old[..., j]
+    return x, y, s, g, h, g_old, h_old, sigma_y, sigma_s, rho_y, rho_s
